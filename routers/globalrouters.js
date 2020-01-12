059234067a1dc88d2eb6a04c2fbbe9ba642daa1b
akashayyappan <48151852+akashayyappan@users.noreply.github.com>
2020-01-12 17:50:38 +0530

    testing webhook

--- routers/globalrouters.js
+++ routers/globalrouters.js
@@ -4,7 +4,7 @@ const userRouter = require("./user");
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.status(200).send("service working");
+    res.status(200).send("service works fine");
 })
 
 router.use("/user",userRouter);
