060a9f5d77b49881f7587798b3110da1af2b7c46
akashayyappan <48151852+akashayyappan@users.noreply.github.com>
2020-01-14 09:07:04 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const globalRouter = require("./routers/globalrouters");
+const cors = require("cors")
 
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 const ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -8,6 +9,7 @@ require("./db/db");
 
 const app = express();
 
+app.use(cors())
 
 app.use(express.json());
 app.use("/", globalRouter);
